81ee00ac70e083ba9512b59ec368a9cc2df54783
Adam <131200917+Corvus-Malus@users.noreply.github.com>
2024-09-20 19:13:37 +0300

    Update install_adguardhome.sh

--- Installer/install_adguardhome.sh
+++ Installer/install_adguardhome.sh
@@ -8,19 +8,41 @@ NC='\033[0m' # Без цвета
 INSTALL_DIR="/opt/etc/AdGuardHome"
 ARCH=$(uname -m)
 
+# Если доступна команда lscpu, используем её для более детального определения архитектуры
+if command -v lscpu > /dev/null; then
+  CPU_ARCH=$(lscpu | grep Architecture | awk '{print $2}')
+else
+  CPU_ARCH=$ARCH
+fi
+
 install() {
-  case $ARCH in
+  case $CPU_ARCH in
     "aarch64")
       URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_arm64.tar.gz"
       ;;
-    "mipsle")
+    "armv5")
+      URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv5.tar.gz"
+      ;;
+    "armv6")
+      URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv6.tar.gz"
+      ;;
+    "armv7")
+      URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_armv7.tar.gz"
+      ;;
+    "mipsle" | "mipsel")
       URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_mipsle_softfloat.tar.gz"
+      ;;
+    "mips64le")
+      URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_mips64le_softfloat.tar.gz"
       ;;
     "mips")
       URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_mips_softfloat.tar.gz"
       ;;
+    "amd64" | "x86_64")
+      URL="https://github.com/AdguardTeam/AdGuardHome/releases/latest/download/AdGuardHome_linux_amd64.tar.gz"
+      ;;
     *)
-      echo -e "${RED}Неподдерживаемая архитектура: $ARCH${NC}" && exit 1
+      echo -e "${RED}Неподдерживаемая архитектура: $CPU_ARCH${NC}" && exit 1
       ;;
   esac
 
@@ -45,34 +67,34 @@ install() {
   echo -e "${GREEN}Скачивание и установка файла автозагрузки...${NC}"
   curl -L -o /opt/etc/init.d/S99adguardhome https://github.com/Corvus-Malus/AdGuardHome/raw/main/Installer/S99adguardhome
   if [ $? -ne 0 ]; then
-    echo -e "${RED}Ошибка при скачивании файла автозагрузки.${NC}" && exit 1
+    echo -е "${RED}Ошибка при скачивании файла автозагрузки.${NC}" && exit 1
   fi
 
   chmod +x /opt/etc/init.d/S99adguardhome
 
-  echo -e "${GREEN}Установка завершена.${NC}"
+  echo -е "${GREEN}Установка завершена.${NC}"
 }
 
 uninstall() {
-  echo -e "${GREEN}Удаление AdGuard Home и связанных файлов...${NC}"
+  echo -е "${GREEN}Удаление AdGuard Home и связанных файлов...${NC}"
 
   # Удаление установленных файлов
   if [ -d "$INSTALL_DIR" ]; then
     rm -rf "$INSTALL_DIR"
-    echo -e "${GREEN}Удален $INSTALL_DIR${NC}"
+    echo -е "${GREEN}Удален $INSTALL_DIR${NC}"
   else
-    echo -e "${RED}AdGuard Home не найден в $INSTALL_DIR${NC}"
+    echo -е "${RED}AdGuard Home не найден в $INSTALL_DIR${NC}"
   fi
 
   # Удаление файла автозагрузки
   if [ -f "/opt/etc/init.d/S99adguardhome" ]; then
     rm /opt/etc/init.d/S99adguardhome
-    echo -e "${GREEN}Удален /opt/etc/init.d/S99adguardhome${NC}"
+    echo -е "${GREEN}Удален /opt/etc/init.d/S99adguardhome${NC}"
   else
-    echo -e "${RED}Файл автозагрузки не найден${NC}"
+    echo -е "${RED}Файл автозагрузки не найден${NC}"
   fi
 
-  echo -e "${GREEN}Удаление завершено.${NC}"
+  echo -е "${GREEN}Удаление завершено.${NC}"
 }
 
 # Проверка аргументов командной строки
@@ -81,6 +103,6 @@ if [ "$1" = "install" ]; then
 elif [ "$1" = "uninstall" ]; then
   uninstall
 else
-  echo -e "${RED}Использование: $0 {install|uninstall}${NC}"
+  echo -е "${RED}Использование: $0 {install|uninstall}${NC}"
   exit 1
 fi
